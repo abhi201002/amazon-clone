e17d3326592de62fa9ce30ea8cd19c1c9a0b4c3e
abhi201002 <2020UEE1234@mnit.ac.in>
2023-01-17 13:18:22 +0530

    Added Firebase

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+Website hosted on Firebase. Visit : https://clone-2c713.web.app/
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,17 +10,19 @@ function Home() {
             <div className="Home_img">
                 <img src= {url} alt="" />
             </div>
-            <div className="product_info">
-              <Product id = "1234"name="Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That The Poor And Middle Class Do Not!: (25th Anniversary Edition)" price="215" img="https://m.media-amazon.com/images/I/81PuKheA8xL._AC_UY327_FMwebp_QL65_.jpg" rating = {3}/>
-              <Product id = "2314"name="Do It Today: Overcome Procrastination, Improve Productivity" price="98" img="https://m.media-amazon.com/images/I/61ZPDQOjw-L._AC_UY327_FMwebp_QL65_.jpg" rating = "4"/>
-            </div>
-            <div className="product_info">
-              <Product id = "5478"name="Apple Watch Ultra GPS + Cellular, 49mm Titanium Case with Orange Alpine Loop - Small" price="85,400" img="https://m.media-amazon.com/images/I/91z5KuonXrL._AC_UY327_FMwebp_QL65_.jpg" rating = {4}/>
-              <Product id = "3412"name='Dell Inspiron 3520 Laptop, Intel Core i5-1235U, Windows 11' price="58,290" img="https://m.media-amazon.com/images/I/51A5zIgX5LL._AC_UY327_FMwebp_QL65_.jpg" rating = {2}/>
-              <Product id = "5132"name="Samsung 80 cm (32 Inches) Wondertainment Series HD Ready LED Smart TV UA32T4340BKXXL (Glossy Black)" price="13,490" img="https://m.media-amazon.com/images/I/71a4ZQNqTiL._AC_UY327_FMwebp_QL65_.jpg" rating = {4}/>
-            </div>
-            <div className="product_info">
-              <Product id = "3344"name="Yonex Arcsaber 71 Light Strung Badminton Racquet, 5UG4 - Gold" price="2,195" img="https://m.media-amazon.com/images/I/61K20GLs86L._AC_UL480_FMwebp_QL65_.jpg" rating = {5}/>
+            <div className="Product">
+              <div className="product_info">
+                <Product id = "1234"name="Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That The Poor And Middle Class Do Not!: (25th Anniversary Edition)" price="215" img="https://m.media-amazon.com/images/I/81PuKheA8xL._AC_UY327_FMwebp_QL65_.jpg" rating = {3}/>
+                <Product id = "2314"name="Do It Today: Overcome Procrastination, Improve Productivity" price="98" img="https://m.media-amazon.com/images/I/61ZPDQOjw-L._AC_UY327_FMwebp_QL65_.jpg" rating = "4"/>
+              </div>
+              <div className="product_info">
+                <Product id = "5478"name="Apple Watch Ultra GPS + Cellular, 49mm Titanium Case with Orange Alpine Loop - Small" price="85,400" img="https://m.media-amazon.com/images/I/91z5KuonXrL._AC_UY327_FMwebp_QL65_.jpg" rating = {4}/>
+                <Product id = "3412"name='Dell Inspiron 3520 Laptop, Intel Core i5-1235U, Windows 11' price="58,290" img="https://m.media-amazon.com/images/I/51A5zIgX5LL._AC_UY327_FMwebp_QL65_.jpg" rating = {2}/>
+                <Product id = "5132"name="Samsung 80 cm (32 Inches) Wondertainment Series HD Ready LED Smart TV UA32T4340BKXXL (Glossy Black)" price="13,490" img="https://m.media-amazon.com/images/I/71a4ZQNqTiL._AC_UY327_FMwebp_QL65_.jpg" rating = {4}/>
+              </div>
+              <div className="product_info">
+                <Product id = "3344"name="Yonex Arcsaber 71 Light Strung Badminton Racquet, 5UG4 - Gold" price="2,195" img="https://m.media-amazon.com/images/I/61K20GLs86L._AC_UL480_FMwebp_QL65_.jpg" rating = {5}/>
+              </div>
             </div>
         </div>
     </>
